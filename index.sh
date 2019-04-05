95c85fb550fb59316a674bc63971f054c3839ef7
GiGaFotress <602330563@qq.com>
2019-04-06 00:57:33 +0800

    服务器搞烂了,偏偏还有人赖着不下播

--- index.sh
+++ index.sh
@@ -12,17 +12,17 @@ fi
 FORMAT="${2:-best}"
 INTERVAL="${4:-100}"
 
-$YOUTUBE=grep "youtube.com" "$1".txt
-$BIL=grep "bilibili.com" "$1".txt|egrep -o "[0-9]{2,}+"
-$TWITCH=grep "twitch.com" "$1".txt
-$TWITCAST=grep "twitcast" "$1".txt|grep -v https://twitcasting.tv/|cut -c 24-
-#$OPENREC=grep "openrec" "$1".txt|grep -v https://www.openrec.tv/user/|cut -c 29-
+YOUTUBE="grep "youtube.com" "$1".txt"
+BIL="grep "bilibili.com" "$1".txt|egrep -o "[0-9]{2,}+""
+TWITCH="grep "twitch.com" "$1".txt"
+TWITCAST="$grep "twitcast" "$1".txt|grep -v https://twitcasting.tv/|cut -c 24-"
+#OPENREC="grep "openrec" "$1".txt|grep -v https://www.openrec.tv/user/|cut -c 29-"
 
 #youtube
 ./record_youtube.sh $YOUTUBE $FORMAT $3 $INTERVAL $SAVEFOLDER &
 #bil    
 ./record_bil.sh $BIL $FORMAT $3 $INTERVAL $SAVEFOLDER&
-#twitch [loop|once] [interval] [savefolder]
+#twitch twitch_id [format] [loop|once] [interval] [savefolder]
 ./record_twitch.sh $TWITCH $FORAMT $3 $INTERVAL $SAVEFOLDER&
 #TWITCAST录制路径仍未解决
 ./record_twitcast.sh $TWITCAST $FORAMT $3 $INTERVAL $SAVEFOLDER&
@@ -37,7 +37,7 @@ wait
 #echo "https://twitcasting.tv/merrysan_cas_?"|grep "twitcast" |cut -c 24-
 
 
-
+#mana best loop 150 /home/centos/Recorder/savevideo/test/
 #./record_youtube.sh https://www.youtube.com/channel/UCHog7L3CzsDg2GH9aza1bPg/live best loop 150 /home/centos/Recorder/savevideo/test/
 #./record_bil.sh 14917277 150 /home/centos/Recorder/savevideo/test/
 #./record_twitch.sh https://www.twitch.tv/yulihong22  best loop 150 /home/centos/Recorder/savevideo/test/
