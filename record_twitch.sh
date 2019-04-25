58e26d84673d62f47a017ebc4cda5c2183cbede0
GiGaFotress <602330563@qq.com>
2019-04-25 19:30:16 +0800

    更新分段录制以及更换老鼠台监测

--- record_twitch.sh
+++ record_twitch.sh
@@ -2,15 +2,14 @@
 # Twitch Live Stream Recorder
 TWITCHPID="$$"
 echo "$6:TWITCH  $TWITCHPID" >> "./running.txt"
-FORMAT="${2:-best}"
-INTERVAL="${4:-110}"
 if [[ ! -n "$1" ]]; then
   echo "usage: $0 twitch_id [format] [loop|once] [interval] [savefolder]"
   exit 1
 fi
 
 # Record the highest quality available by default
-
+FORMAT="${2:-best}"
+INTERVAL="${4:-110}"
 
 while true; do
   # Monitor live streams of specific channel
@@ -19,15 +18,12 @@ while true; do
     echo "$LOG_PREFIX Try to get current live stream of twitch.tv/$1"
 
     # Get the m3u8 address with streamlink
-    M3U8_URL=$(streamlink --stream-url "https://www.twitch.tv/$1" "$FORMAT")
-    (echo "$M3U8_URL" | grep -q ".m3u8") && break
-
+	curl -s https://api.twitch.tv/kraken/streams/$1?client_id=(twitchclientid)|grep -q live&& break
     echo "$LOG_PREFIX The stream is not available now."
     echo "$LOG_PREFIX Retry after $INTERVAL seconds..."
     sleep $INTERVAL
   done
-
-	TITLE=$(youtube-dl --get-description "https://www.twitch.tv/$1")
+  TITLE=$(youtube-dl --get-description "https://www.twitch.tv/$1")
   ID=$(youtube-dl --get-id "https://www.twitch.tv/$1")
 
   # Record using MPEG-2 TS format to avoid broken file caused by interruption
@@ -36,7 +32,8 @@ while true; do
   echo "$LOG_PREFIX Use command \"tail -f $FNAME.log\" to track recording progress."
 
   # Start recording
-  ffmpeg -re -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 8  -i "$M3U8_URL" -codec copy -f mpegts "$5$FNAME" > "$5$FNAME.log" 2>&1
+   M3U8_URL=$(streamlink --stream-url "https://www.twitch.tv/$1" "$FORMAT")
+  ffmpeg -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 8  -i "$M3U8_URL" -codec copy   -f hls -hls_time 3600 -hls_list_size 0 "$5$FNAME" > "$5$FNAME.log" 2>&1
 
   # Exit if we just need to record current stream
   LOG_PREFIX=$(date +"[%Y-%m-%d %H:%M:%S]")
