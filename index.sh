0f9386cdd977aa4625bdf5ad33b32d583d8adf9a
GiGaFotress <602330563@qq.com>
2019-04-27 22:10:52 +0800

    预备新的分组

--- index.sh
+++ index.sh
@@ -15,32 +15,47 @@ YOUTUBE=$(grep "youtube.com" ./config/"$1".txt|cut -c 33-)
 BIL=$(grep "bilibili.com" ./config/"$1".txt|egrep -o "[0-9]{2,}+")
 TWITCH=$(grep "twitch.tv" ./config/"$1".txt|cut -c 23-)
 TWITCAST=$(grep "twitcast" ./config/"$1".txt|cut -c 24-)
+YOUTUBESAVE=$(youtube/)
+BILSAVE=$(bil/)
+TWITCHSAVE=$(twitch/)
+TWITCASTSAVE=$(twitchcast/)
+
+
+
 #OPENREC=$(grep "openrec" ./config/"$1".txt|grep -v https://www.openrec.tv/user/|cut -c 29-)
 
-if [[ ! -n "$SAVEFOLDER" ]]; then 
-  echo "no output dir"
-  exit 1
-fi
+
 
 if [ ! -d "$SAVEFOLDER" ]; then
   mkdir $SAVEFOLDER
 fi
 
 #youtube
+if [ ! -d "$SAVEFOLDER$YOUTUBESAVE" ]; then
+  mkdir $SAVEFOLDER$YOUTUBESAVE
+fi
 [[ -n "$YOUTUBE" ]] && ./record_youtube.sh $YOUTUBE $FORMAT $LOOP $INTERVAL $SAVEFOLDER $1 &
 sleep 10
 #bil    
-[[ -n "$BIL" ]] && ./record_bil.sh $BIL $FORMAT $LOOP $INTERVAL $SAVEFOLDER $1 &
+if [ ! -d "$SAVEFOLDER$BILSAVE" ]; then
+  mkdir $SAVEFOLDER$BILSAV
+fi
+[[ -n "$BIL" ]] && ./record_bil.sh $BIL $FORMAT $LOOP $INTERVAL $SAVEFOLDER$BILSAVE $1 &
 sleep 10
 #twitch twitch_id [format] [loop|once] [interval] [savefolder]
-[[ -n "$TWITCH" ]] && ./record_twitch.sh $TWITCH $FORMAT $LOOP $INTERVAL $SAVEFOLDER $1 &
+if [ ! -d "$SAVEFOLDER$TWITCHSAVE" ]; then
+  mkdir $SAVEFOLDER$TWITCHSAVE
+fi
+[[ -n "$TWITCH" ]] && ./record_twitch.sh $TWITCH $FORMAT $LOOP $INTERVAL $SAVEFOLDER$TWITCHSAVE $1 &
 sleep 10
 #TWITCAST
-
-if [ ! -d "$SAVEFOLDER/livedl" ]; then
-  cp ./livedl $SAVEFOLDER
+if [ ! -d "$SAVEFOLDER$TWITCASTSAVE" ]; then
+  mkdir $SAVEFOLDER$TWITCASTSAVE
 fi
-[[ -n "$TWITCAST" ]] && ./record_twitcast.sh $TWITCAST  $LOOP $INTERVAL $SAVEFOLDER $1 &
+if [ ! -d "$SAVEFOLDER/livedl" ]; then
+  cp ./livedl $SAVEFOLDER$TWITCASTSAVE
+fi
+[[ -n "$TWITCAST" ]] && ./record_twitcast.sh $TWITCAST  $LOOP $INTERVAL $SAVEFOLDER$TWITCASTSAVE $1 &
 #OPENREC
 #./record_openrec.sh $OPENRCE $FORAMT $LOOP $INTERVAL $SAVEFOLDER&
 wait
