342d7dc38615e98442d50cdc4bb94420c72fa771
GiGaFotress <602330563@qq.com>
2019-04-28 19:19:51 +0800

    一键启动脚本有关键部分bug,勿用

--- lazyrun.sh
+++ lazyrun.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-
-
+#!/usr/bin/env bash
+#cd /home/centos/Recorder/config
+#ls|grep .txt|sed 's/.txt//g'
+for ((NUM=$(ls /home/centos/Recorder/config|grep -c .txt); NUM>0; --NUM)) 
+do
+VTBNAME=$(ls /home/centos/Recorder/config|grep .txt|sed 's/.txt//g'|sed -n "$NUM"p)
+sleep 3
+screen -S $VTBNAME /home/centos/Recorder/index.sh $VTBNAME
+sleep 3
+screen -d $VTBNAME
+done
