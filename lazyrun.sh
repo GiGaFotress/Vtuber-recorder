c6de457466d84bc9fb88bdeed25c7771677c282e
GiGaFotress <602330563@qq.com>
2019-04-29 10:58:01 +0800

    Update lazyrun.sh

--- lazyrun.sh
+++ lazyrun.sh
@@ -3,9 +3,9 @@
 #ls|grep .txt|sed 's/.txt//g'
 for ((NUM=$(ls /home/centos/Recorder/config|grep -c .txt); NUM>0; --NUM)) 
 do
-VTBNAME=$(ls /home/centos/Recorder/config|grep .txt|sed 's/.txt//g'|sed -n "$NUM"p)
-sleep 3
-screen -S $VTBNAME /home/centos/Recorder/index.sh $VTBNAME
-sleep 3
-screen -d $VTBNAME
+NAME=$(ls /home/centos/Recorder/config|grep .txt|sed 's/.txt//g'|sed -n "$NUM"p)
+sleep 1
+screen -dmS $NAME /home/centos/Recorder/index.sh $NAME
+sleep 1
+echo $NAME
 done
