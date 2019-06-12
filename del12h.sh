f21d321b5dca089b0d680d5a786d74267e5c7833
GiGaFotress <602330563@qq.com>
2019-06-12 09:35:12 +0800

    Update del12h.sh

--- del12h.sh
+++ del12h.sh
@@ -1,4 +1,4 @@
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +240 -exec ls -l {} \ >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +240 -exec ls -l {} \ > "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +240 -delete >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -exec ls -l {} \ >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -delete >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
