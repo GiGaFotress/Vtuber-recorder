e39485cc1432fd81c2e95d40afe1e81aca2d9b46
GiGaFotress <602330563@qq.com>
2019-06-12 09:15:02 +0800

    Update rcloneupload.sh

--- rcloneupload.sh
+++ rcloneupload.sh
@@ -1,7 +1,7 @@
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3    -name "*.*"  -size 0 -type f  -exec ls -l  {} \ >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find /home/centos/Vtuber-recorder/savevideo/ -maxdepth 3 -name "*.*" -size 0 -type f -delete >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find /home/centos/Vtuber-recorder/ -maxdepth 3    -name "*.ts.log"   -type f -size -35k  -exec ls -l  {} \ >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find /home/centos/Vtuber-recorder/ -maxdepth 3    -name "*.ts.log"   -type f -size -35k  -delete  >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find /home/centos/Vtuber-recorder/ -maxdepth 3    -name "*.ts"   -type f -size 1M  -exec ls -l  {} \ >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find /home/centos/Vtuber-recorder/ -maxdepth 3    -name "*.ts"   -type f -size 1M  -delete >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-rclone move /home/centos/Vtuber-recorder/savevideo savevideo:/path/ --include *.ts  --buffer-size 32M  --transfers 6 --low-level-retries 200 -v >> "$RECFOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3    -name "*.*"  -size 0 -type f  -exec ls -l  {} \ >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 3 -name "*.*" -size 0 -type f -delete >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/ -maxdepth 3    -name "*.ts.log"   -type f -size -35k  -exec ls -l  {} \ >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/ -maxdepth 3    -name "*.ts.log"   -type f -size -35k  -delete  >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/ -maxdepth 3    -name "*.ts"   -type f -size 1M  -exec ls -l  {} \ >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find FOLDER/Vtuber-recorder/ -maxdepth 3    -name "*.ts"   -type f -size 1M  -delete >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+rclone move FOLDER/Vtuber-recorder/savevideo savevideo:/path/ --include *.ts  --buffer-size 32M  --transfers 6 --low-level-retries 200 -v >> "FOLDER/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
