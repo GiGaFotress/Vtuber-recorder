ad1f3ebff63e665c934d8b2b00d980a04c2fad83
GiGaFotress <602330563@qq.com>
2019-06-12 09:15:39 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 4    -name "*.*"  -size 0 -type f  -exec ls -l  {} \;
-find /home/centos/Vtuber-recorder/savevideo/ -maxdepth 4  -name "*.*" -size 0 -type f -delete ;
-find /home/centos/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -exec ls -l  {} \;
-find /home/centos/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -delete  ;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 4    -name "*.*"  -size 0 -type f  -exec ls -l  {} \;
+find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 4  -name "*.*" -size 0 -type f -delete ;
+find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -exec ls -l  {} \;
+find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -delete  ;
