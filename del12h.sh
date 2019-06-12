9910049e50f63f29e6c0887b421381977fa654c5
GiGaFotress <602330563@qq.com>
2019-06-12 09:16:31 +0800

    Update del12h.sh

--- del12h.sh
+++ del12h.sh
@@ -1,11 +1,11 @@
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +240 -exec ls -l {} \;
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +240 -exec rm  {} \;
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -exec ls -l {} \;
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -delete ;
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -exec ls -l {} \;
-find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -delete ;
-find /home/centos/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \;
-find /home/centos/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -delete ;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +240 -exec ls -l {} \;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +240 -exec rm  {} \;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -exec ls -l {} \;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -delete ;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -exec ls -l {} \;
+find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -delete ;
+find FOLDER/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \;
+find FOLDER/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -delete ;
 
 
 
