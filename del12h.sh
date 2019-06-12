d361601e9dd517e17054ecc5dde9c1e2c391c0ab
GiGaFotress <602330563@qq.com>
2019-06-12 05:30:29 +0000

    Update del12h.sh

--- del12h.sh
+++ del12h.sh
@@ -1,11 +1,11 @@
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +240 -exec ls -l {} \ > "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +240 -delete >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -exec ls -l {} \ >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -delete >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -exec ls -l {} \ >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -delete >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \ >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find FOLDER/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -delete >> "FOLDER/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +240 -exec ls -l {} \ > "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +240 -delete >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -exec ls -l {} \ >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -delete >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -exec ls -l {} \ >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -delete >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \ >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -delete >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 
 
 
