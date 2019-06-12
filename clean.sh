49989a5de9cb5e1e27922f9b524231a4a6fe8dc9
GiGaFotress <602330563@qq.com>
2019-06-12 05:31:06 +0000

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-find FOLDER/Vtuber-recorder/savevideo/  -maxdepth 4    -name "*.*"  -size 0 -type f  -exec ls -l  {} \;
-find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 4  -name "*.*" -size 0 -type f -delete ;
-find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -exec ls -l  {} \;
-find FOLDER/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -delete  ;
+find folder/Vtuber-recorder/savevideo/  -maxdepth 4    -name "*.*"  -size 0 -type f  -exec ls -l  {} \;
+find folder/Vtuber-recorder/savevideo/ -maxdepth 4  -name "*.*" -size 0 -type f -delete ;
+find folder/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -exec ls -l  {} \;
+find folder/Vtuber-recorder/savevideo/ -maxdepth 4    -name "*.ts.log"   -type f -size -35k  -delete  ;
