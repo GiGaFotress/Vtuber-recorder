43de43512d90e82d413b6fbde192bfb05d8516e9
GiGaFotress <602330563@qq.com>
2019-06-12 09:10:26 +0800

    Update del12h.sh

--- del12h.sh
+++ del12h.sh
@@ -2,6 +2,8 @@ find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f
 find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +240 -exec rm  {} \;
 find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -exec ls -l {} \;
 find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +240 -delete ;
+find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -exec ls -l {} \;
+find /home/centos/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +240 -delete ;
 find /home/centos/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \;
 find /home/centos/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -delete ;
 
