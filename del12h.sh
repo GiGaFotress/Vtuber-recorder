bc9484a59aae264662f151d4df745f6332d003f0
GiGaFotress <602330563@qq.com>
2019-07-31 15:15:59 +0000

    添加自动更新py软件

--- del12h.sh
+++ del12h.sh
@@ -6,6 +6,9 @@ find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f
 find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.info.txt"  -type f -mmin +720 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/log/ -maxdepth 1  -name "*.log*"  -type f -mmin +6000 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+pip3 install  --upgrade youtube-dl
+pip3 install  --upgrade you-get
+pip3 install  --upgrade streamlink
 
 
 
