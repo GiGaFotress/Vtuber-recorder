eb6c9935900e698924cecf5813789219fac6fe88
GiGaFotress <602330563@qq.com>
2019-08-04 10:26:51 +0000

    导入环境变量

--- del12h.sh
+++ del12h.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+. /etc/profile
+. ~/.bash_profile
 find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"  -type f -mmin +720 -exec ls -l {} \; > "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts"   -type f -mmin +720 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/savevideo/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +720 -exec ls -l {} \; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
