3c70b164eda9dabc5989b2a631332ae1ecfa2cc6
GiGaFotress <602330563@qq.com>
2019-08-11 16:58:21 +0000

    Update rcloneupload.sh

--- rcloneupload.sh
+++ rcloneupload.sh
@@ -4,4 +4,4 @@ find folder/Vtuber-recorder/ -maxdepth 3    -name "*.ts.log"   -type f -size -35
 find folder/Vtuber-recorder/ -maxdepth 3    -name "*.ts.log"   -type f -size -35k  -delete;  >> "folder/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/ -maxdepth 3    -name "*.ts"   -type f -size 1M  -exec ls -l  {} \; >> "folder/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/ -maxdepth 3    -name "*.ts"   -type f -size 1M  -delete; >> "folder/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
-rclone move folder/Vtuber-recorder/savevideo savevideo:/path/ --include *.ts  --buffer-size 32M  --transfers  4 --retries 6  --retries-sleep 2m --low-level-retries 300  -v >> "folder/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
+rclone move folder/Vtuber-recorder/savevideo savevideo:/path/ --include *.ts  --buffer-size 32M  --transfers  2 --retries 6  --retries-sleep 2m --low-level-retries 300  -v >> "folder/Vtuber-recorder/log/rclone$(date +"[%Y-%m-%d-%H]").log" 2>&1
