ef9487001a9b968717b99b419fae0318074d8cee
GiGaFotress <602330563@qq.com>
2019-12-31 16:16:50 +0000

    Update del12h.sh

--- del12h.sh
+++ del12h.sh
@@ -8,8 +8,8 @@ find folder/Vtuber-recorder/savevideo/  -maxdepth 5 -name "*.ts.*"  -type f -mmi
 find folder/Vtuber-recorder/savevideo/  -maxdepth 5 -name "*.ts.*"  -type f -mmin +720 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/savevideo/  -maxdepth 5 -name "*.info.txt"  -type f -mmin +720 -exec ls -l {} \; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 find folder/Vtuber-recorder/savevideo/  -maxdepth 5 -name "*.info.txt"  -type f -mmin +720 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find folder/Vtuber-recorder/ -maxdepth 5 -name "*.log*"  -type f -mmin +6000 -exec ls -l {} \; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
-find folder/Vtuber-recorder/ -maxdepth 5 -name "*.log*"  -type f -mmin +6000 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/ -maxdepth 5 -name "*.log*"  -type f -mmin +12000 -exec ls -l {} \; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
+find folder/Vtuber-recorder/ -maxdepth 5 -name "*.log*"  -type f -mmin +12000 -delete; >> "folder/Vtuber-recorder/log/del12h$(date +"[%Y-%m-%d-%H]").log" 2>&1
 pip3 install  --upgrade youtube-dl
 pip3 install  --upgrade you-get
 pip3 install  --upgrade streamlink
