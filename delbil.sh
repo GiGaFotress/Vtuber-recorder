b8a7cce9f7669b029e0db21b16c01d9531c06d1b
GiGaFotress <602330563@qq.com>
2019-12-31 16:33:06 +0000

    Update delbil.sh

--- delbil.sh
+++ delbil.sh
@@ -1,4 +1,4 @@
-find /home/centos/Vtuber-recorder/savevideo/*/bil/  -maxdepth 3  -name "*.ts"  -type f -mmin +90 -exec ls -l {} \;
-find /home/centos/Vtuber-recorder/savevideo/*/bil/ -maxdepth 3  -name "*.ts"   -type f -mmin +90-exec rm  {} \;
-find /home/centos/Vtuber-recorder/savevideo/*/bil/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +90 -exec ls -l {} \;
-find /home/centos/Vtuber-recorder/savevideo/*/bil/  -maxdepth 3  -name "*.ts.*"  -type f -mmin +90 -delete ;
+find folder/Vtuber-recorder/savevideo/*/bil/  -maxdepth 5  -name "*.ts"  -type f -mmin +90 -exec ls -l {} \;
+find folder/Vtuber-recorder/savevideo/*/bil/ -maxdepth 5  -name "*.ts"   -type f -mmin +90-exec rm  {} \;
+find folder/Vtuber-recorder/savevideo/*/bil/  -maxdepth 5  -name "*.ts.*"  -type f -mmin +90 -exec ls -l {} \;
+find folder/Vtuber-recorder/savevideo/*/bil/  -maxdepth 5  -name "*.ts.*"  -type f -mmin +90 -delete ;
